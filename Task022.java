a553658f83c865967858b85b24a22fce77d3240d
avinashhosagoudar <avinashbaleshhosagoudar@gmail.com>
2025-06-06 12:44:15 +0530

    day 9 java files

---
+++ Task022.java
@@ -0,0 +1,11 @@
+public class Task022 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+
+    }
+}
